442ee8e4ba7a7fd3a0b9c4290bc904d025c834dd
Saul Woolf <saulvhwoolf@gmail.com>
2024-08-15 23:49:47 -0400

    Added gallery view

--- next-undrian/app/gallery/page.js
+++ next-undrian/app/gallery/page.js
@@ -1,16 +1,23 @@
 import Gallery from "@/components/Gallery";
 
 async function getGalleryData() {
-    const origin = typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000';
-    const response = await fetch(`${origin}/api/getGalleryData`, {method: "GET"});
-    const data = await response.json();
-
-    return data
+    try {
+        const origin = typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000';
+        const response = await fetch(`${origin}/api/getGalleryData`, {method: "GET"});
+        return await response.json();
+    } catch(e) {
+        return null
+    }
+    // return data
 }
 
 export default async function GalleryPage() {
     const galleryData = await getGalleryData()
-
+    if (galleryData == null) {
+        return (
+            <h1>UH OH.... :(</h1>
+        )
+    }
     return (
         <div>
             {/*<div style={{aspectRatio:"1", maxHeight: "100vh", maxWidth: "100vw", margin: "0 auto"}}>*/}
